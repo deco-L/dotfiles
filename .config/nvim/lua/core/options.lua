0b19678c332c7b67d22f60da7245060ac3f48dba
deco-L <scotchwitchbroom@gmail.com>
2026-01-09 10:17:09 +0900

    🚧 WIP

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -9,3 +9,8 @@ vim.o.mouse = ""
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.list = false
+vim.opt.showbreak = ">\\"
