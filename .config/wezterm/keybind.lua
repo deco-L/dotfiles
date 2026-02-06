22ce60c43324baebdb4b99e9421c3366af6ba02b
deco-L <scotchwitchbroom@gmail.com>
2026-02-07 00:02:33 +0900

    fix(wezter): fix non return value (keybinds.lua) 

--- .config/wezterm/keybind.lua
+++ .config/wezterm/keybind.lua
@@ -1,9 +1,8 @@
 local module = {}
 local search_mode = nil
 
-function module.apply_to_config(config, act)
-	config.key_tables = config.key_tables or {}
-	config.key_tables.search_mode = {
+local function search_mode_keybinds(key_tables, act)
+	key_tables.search_mode = {
 		{ key = "Enter", mods = "NONE", action = act.CopyMode("PriorMatch") },
 		{ key = "Escape", mods = "NONE", action = act.CopyMode("Close") },
 		{ key = "[", mods = "CTRL", action = act.CopyMode("Close") },
@@ -25,3 +24,11 @@ function module.apply_to_config(config, act)
 		{ key = "DownArrow", mods = "NONE", action = act.CopyMode("NextMatch") },
 	}
 end
+
+function module.apply_to_config(config, act)
+	config.key_tables = config.key_tables or {}
+
+	search_mode_keybinds(config.key_tables, act)
+end
+
+return module
